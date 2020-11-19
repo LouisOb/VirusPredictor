ecf61c1dbe524bab533f8b74f4bc535225c7bf01
louis <louis@physcip.uni-stuttgart.de>
2020-11-19 10:46:03 +0100

    housekeeping

--- README.md
+++ README.md
@@ -25,15 +25,32 @@ SARS_2_list=virus_species(DNA_SARS_COV_2)
 SARS_2_list_plot = SARS_2_list.na_count(Tup)
 ```
 
-## Fingerprint.py
-Contains the functions to identify a common pattern of nucleobases and aminoacids after translation in a virus DNA sequence.
-A fasta file which contains the Virus DNA sequences has to be passed to the functions.
-
+## Clustering.py
+Contains the funtions to extract features from nucleobase combinations of virus genome sequences. The combination of featurized tuples is passed to initialize the clustering object.
 ```python
-import Fingerprint
+import clustering
 
-Fingerprint.ORF_scan("SARS-CoV-1.fasta.txt")
-Fingerprint.test_identity("SARS-CoV-1.fasta.txt")
+#Introduce Tuple that are searched for in the sequence
+	NA = ["A","C","G","T"]
+	Tup = []
+	for na1 in NA:
+		for na2 in NA:
+			for na3 in NA:
+						Tup.append(na1+na2+na3)
+
+	viruslist=clustering(Tup)
+	viruslist.add(["SARS_COV_2_new_new.fasta","SARS-CoV-1.fasta.txt","MERS.fasta","Bovine_coronavirus.fasta","Camel_alphacoronavirus.fasta","Duck_coronavirus.fasta"],["SARS CoV 2","SARS","MERS","Bovine corona virus","Camel alpha corona virus","Duck corona virus"])
+	viruslist.ORF_tuples(11000)
+	viruslist.PCA(2,True)
+	viruslist.plot("Coronafamily_test.png")
 ```
 
-It will then print the percentage of the sequences in the file containing the discovered pattern.
+[//]: # ## Fingerprint.py
+[//]: #Contains the functions to identify a common pattern of nucleobases and aminoacids after translation in a virus DNA sequence.
+[//]: #A fasta file which contains the Virus DNA sequences has to be passed to the functions.
+[//]: # ```python
+[//]: # import Fingerprint
+[//]: # Fingerprint.ORF_scan("SARS-CoV-1.fasta.txt")
+[//]: # Fingerprint.test_identity("SARS-CoV-1.fasta.txt")
+[//]: # ```
+[//]: # It will then print the percentage of the sequences in the file containing the discovered pattern.
--- clustering.py
+++ clustering.py
@@ -10,6 +10,33 @@ from virus import virus, virus_species, readfile
 from os import listdir
 
 class clustering:
+	"""
+	The class with the means to extract features based on nucleobase combinations and PCA.
+
+	A clustering class object is able to load fasta files and extract features based on a
+	preselected set of nucleobase combinations. The features are extracted by counting the
+	occurences of a nucleobase combination and dividing it by the size of the whole
+	sequence.
+	PCA analysis can then be applied to the vector containing the normalized count to
+	reduce the dimensionality to a selected value.
+	Attributes:	
+		DNA_set: 	A list containing lists for each virus file supplied containing
+				 	dictionaries with the DNA sequences of the viruses.
+		viruslist:	A list containing objects of the class virus.
+		colors:		A list containing plotting colors.
+		plot_colors:The colors used when calling the plotting function.
+		tuples:		The list containing the nucleobase combinations which are counted.
+		tuple_count:The list containing the count of the nucleobase combinations for the
+					supplied virus sequences.
+		orf_count:	The list containing the count of nucleobase combinations from ORFs of
+					virus sequences.
+		plot_list:	The list including the two list to be plotted.
+		names:		The list containing the names of the fasta files or a supplied name.
+		mode:		A string which saves the operation used on the set. Possible values are
+					"orf" if the extraction has been executed of an ORF or "tuples" if the whole
+					sequence has been analysed.
+	
+	"""
 	DNA_set		= []
 	viruslist	= None
 	colors		= ["green","red","blue","black","purple","orange","pink","yellow","darkgoldenrod","deepskyblue","grey","lime","cyan"]
@@ -25,6 +52,18 @@ class clustering:
 		self.tuples=tuples_list
 
 	def add(self,filename,name=[]):
+		"""
+		The Function which adds virus files to the set.
+		
+		This function loads fasta formated files and adds them to the list of virus sequences.
+		Args:
+			filename: Either a string or a list containing the name or names of the fasta
+					  file/s with the virus sequence/s.
+			name:	  A string or list with the name/s to be assigned to the virus 
+					  sequence/s from the fasta files/s.
+		Returns:
+			Nothing
+		"""
 		if(type(filename)==str):
 			if(name==[]):
 				self.names.append([filename,filename])
@@ -44,6 +83,18 @@ class clustering:
 				self.DNA_set.append(DNA)
 
 	def tuples(self):
+		"""
+		The Function which counts the nucleobase combinations.
+		
+		This function counts the nucleobase combinations in the whole sequence of all
+		virus sequences contained in the object viruslist. The extracted features are
+		stored in the list tuple_count.
+		Args:
+			None
+		Returns:
+			Nothing
+		"""
+		
 		self.clean()
 		self.mode = "tuples"
 		for listing in self.DNA_set:
@@ -63,6 +114,17 @@ class clustering:
 			color_index+=1
 	
 	def ORF_tuples(self,length=12000):
+		"""
+		The Function which counts the nucleobase combinations in an ORF.
+		
+		This function counts the nucleobase combinations in ORFs of a specified size
+		of all virus sequences contained in the object viruslist. The extracted 
+		features are stored in the list tuple_count.
+		Args:
+			lenght: The minimum length of the ORF sequence used to extract features.
+		Returns:
+			Nothing
+		"""
 		self.clean()
 		self.mode = "orf"
 		for listing in self.DNA_set:
@@ -82,6 +144,17 @@ class clustering:
 			color_index+=1
 
 	def PCA(self,components,use_old=False):
+		"""
+		The Function which carries out PCA.
+		
+		This function uses PCA on the list tuple_count and saves the results to the
+		plot_list.
+		Args:
+			components: The selected dimensionality the result has to have after PCA.
+			use_old:	Use a PCA Matrix saved in an older run to transform the set.
+		Returns:
+			Nothing
+		"""
 		if(self.mode=="tuples"):
 			pca_list=np.hstack(self.tuple_count)
 		else:
@@ -95,6 +168,17 @@ class clustering:
 			pk.dump(pca.components_,open("Mat_pca.pk","wb"))
 
 	def plot(self,filename,title=""):
+		"""
+		The Function which plots two dimensional data.
+		
+		This function uses the matplotlib to plot the two dimensional data contained
+		in the list plot_list.
+		Args:
+			filename: The filename which the image will be saved to.
+			title:	  The title of the matplotlib plot.
+		Returns:
+			Nothing
+		"""
 		if(not len(self.plot_list)):
 			print("[Warning] No plot list availiable for object")
 			return
@@ -112,9 +196,30 @@ class clustering:
 		plt.close()
 
 	def load(self,filename):
+		"""
+		The Function which loads pickle files.
+	
+		The function used to load pickle files containing information or
+		results saved in previous runs.
+
+		Args:
+			filename: The name of the pickle file.
+		Returns:
+			Nothing
+		"""
 		return pk.load(open("save/"+filename,"rb"))
 		
 	def save(self):
+		"""
+		The Function which saves to pickle files.
+	
+		The function used to save extraced results to pickle files.
+
+		Args:
+			None
+		Returns:
+			Nothing
+		"""
 		for index in range(len(self.names)):
 			if(self.tuple_count==[]):
 				dump_array = [[],self.orf_count[index]]
@@ -125,6 +230,16 @@ class clustering:
 			pk.dump(dump_array,open("save/"+self.names[index][1]+".pk","wb"))
 	
 	def clean(self):
+		"""
+		A Function which clears some lists
+	
+		This function clears some lists which enabels a new feature extraction.
+
+		Args:
+			None
+		Returns:
+			Nothing
+		"""
 		self.viruslist		= []
 		self.plot_colors	= []
 		self.tuple_count	= []
@@ -141,16 +256,12 @@ if __name__ == "__main__":
 						Tup.append(na1+na2+na3)
 
 	testlist=clustering(Tup)
-#	testlist.add("Duck_coronavirus.fasta")
 	testlist.add(["SARS_COV_2_new_new.fasta","SARS-CoV-1.fasta.txt","MERS.fasta","Bovine_coronavirus.fasta","Camel_alphacoronavirus.fasta","Duck_coronavirus.fasta"],["SARS CoV 2","SARS","MERS","Bovine corona virus","Camel alpha corona virus","Duck corona virus"])
-#	testlist.add(["SARS_COV_2_new.fasta","SARS_COV_1.fasta","MERS.fasta"],["SARS CoV 2","SARS","MERS"])
-#	testlist.add(["Alpha_Herpes_virus.fasta","Beta_Herpes_virus.fasta","Gamma_Herpes_virus.fasta"],["Alpha herpes virus","Beta herpes virus","Gamma herpes virus"])
-#	testlist.add(["Mastadeno_A.fasta","Mastadeno_B.fasta","Mastadeno_C.fasta","Mastadeno_D.fasta","Mastadeno_E.fasta","Mastadeno_F.fasta"],["Mastadeno A virus", "Mastadeno B virus", "Mastadeno C virus", "Mastadeno D virus", "Mastadeno E virus", "Mastadeno F virus"])
-#	testlist.add(["Vaccinia_virus.fasta","Variola_virus.fasta","Cowpox_virus.fasta"],["Vaccinia virus", "Variola virus", "Cowpox virus"])
 	testlist.ORF_tuples(11000)
 	testlist.PCA(2,True)
-	testlist.plot("SARSfamily_ORF11000.png")
-#	testlist.plot("herpes_corona_family.png")
+	testlist.plot("Coronafamily_test.png")
+
+
 
 
 
